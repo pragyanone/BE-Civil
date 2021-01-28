377d8cf284948bb61e0b9b3debd635dda0da377c
Pragyan Shrestha <pragyanone@gmail.com>
2021-01-28 11:48:46 -0800

    Update alternate_depths.py

--- 4th sem/hydraulics/Computer Codes/alternate_depths.py
+++ 4th sem/hydraulics/Computer Codes/alternate_depths.py
@@ -9,9 +9,9 @@ E = float(input('Enter the specific energy \n'))
 Q = float(input('Enter the discharge \n'))
 
 # Defining Quantities
-g = 9.81												# Acceleration due to gravity
-yc = (Q ** 2 / (b ** 2 * g)) ** (1/3)					# Depth at critical flow
-y = yc													# Dummy variable for iteration
+g = 9.81					# Acceleration due to gravity
+yc = (Q ** 2 / (b ** 2 * g)) ** (1/3)		# Depth at critical flow
+y = yc						# Dummy variable for iteration
 RHS = y + Q ** 2 / (b ** 2 * y ** 2 * 2 * g)
 
 # Iteration for super-critical flow
@@ -22,13 +22,13 @@ while abs(E - RHS) > 0.01:
 		break		
 
 # Iteration for sub-critical flow
-y1 = y 													# super-critical depth
+y1 = y 					# super-critical depth
 y = yc
 while abs(E - RHS) > 0.01:
 	y += 0.0001
 	RHS = y + Q ** 2 / (b ** 2 * y ** 2 * 2 * g)
 	if abs(y) > 15 * abs(yc):
 		break		
-y2 = y													# super-critical depth	
+y2 = y					# sub-critical depth	
 print('\n \nyc = %.3f' %(yc))
 print('super-critical depth = %.3f \nsub-critical depth = %.3f' %(y1, y2))
